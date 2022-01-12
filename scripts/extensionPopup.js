977075ecf58a247f2ef010d59c431f4fb61d8b08
Drumnadrochit <drumnadrochit@mail.de>
2022-01-12 13:25:04 +0100

    AZ-11: Add communication with backend

--- scripts/extensionPopup.js
+++ scripts/extensionPopup.js
@@ -2,32 +2,38 @@ let deviceDropdown = document.getElementById('device-select')
 let closeDeviceWindow = document.getElementById('close-device-window')
 let addDeviceWindow = document.getElementById('add-device-window')
 let device = deviceDropdown.value
-let timer
+let tries = 0
+let refreshTimer
 
 deviceDropdown.addEventListener('change', () => {
   console.log(deviceDropdown.value)
   if (deviceDropdown.value === 'add') {
     deviceDropdown.value = device
-    timer = window.setInterval(updateQRCode, 25000)
+    refreshTimer = window.setInterval(updateQRCode, 25000)
     updateQRCode()
 
     addDeviceWindow.hidden = false
   }
 })
 
-closeDeviceWindow.addEventListener('click', () => {
-  addDeviceWindow.hidden = true
-  clearInterval(timer)
-  console.log('jona du hs')
-})
-
 function updateQRCode() {
+  if (tries >= 5) {
+    stopAddDevice()
+
+    return
+  }
+  tries++
   let div = document.getElementById('qrcode')
   div.innerHTML = ''
 
   let code = generateQRCode()
   console.log(code)
-
+  {
+    let c = code
+    let timer = window.setInterval(() => {
+      checkCode(c, timer)
+    }, 500)
+  }
   let qrcode = new QRCode(div, {
     text: code,
     width: 250,
@@ -41,7 +47,6 @@ function updateQRCode() {
 
 function generateQRCode() {
   let response = JSON.parse(httpGet(`${Config.HOST}:${Config.PORT}/api/v1/qr/generate`))
-  console.log(response)
   if (!response.success) {
     console.log('Error on /api/v1/qr/generate:')
     console.log(response.error)
@@ -52,4 +57,32 @@ function generateQRCode() {
   let uuid = response.uuid
 
   return uuid
+}
+
+function checkCode(code, timer) {
+  let response = httpPost(`${Config.HOST}:${Config.PORT}/api/v1/qr/status`, `{ "code": "${code}" }`)
+  if (!response || !response.success) {
+    clearInterval(timer)
+
+    return
+  }
+  if (response.status === 'registered' && response.browserToken && response.deviceName) {
+    clearInterval(timer)
+
+    addDevice(response.browserToken, response.deviceName)
+    stopAddDevice()
+  }
+}
+
+function stopAddDevice() {
+  tries = 0
+  addDeviceWindow.hidden = true
+  clearInterval(refreshTimer)
+  // to implement later
+  console.log('stop add device')
+}
+
+function addDevice(browserToken, deviceName) {
+  // to implement later
+  console.log(`add device: browserToken: ${browserToken} deviceName:${deviceName}`)
 }
