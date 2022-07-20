a34d03b8c7e6e10b491c2f5a716ef9a356629c62
Ancocodet <ancoplays@gmail.com>
2022-07-20 12:40:03 +0200

    Implement Backwards compatibility

---
+++ scripts/extension/background.js
@@ -0,0 +1,34 @@
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    // TODO: change to Promise based way, as sendResponse is deprecated
+    try {
+      if (!(message.cmd === 'download')) {
+        return
+      }
+  
+      let browserToken = DeviceManager.getSelection()
+  
+      if (!browserToken) {
+        throw 'No device connected'
+      }
+  
+      let videoDetails = message.video
+  
+      let body = {
+        browserToken: browserToken,
+        title: videoDetails.title,
+        author: videoDetails.author,
+        videoId: videoDetails.videoId,
+        quality: videoDetails.quality
+      }
+  
+      let json = httpPost(`${Config.HOST}/download`, JSON.stringify(body))
+  
+      if (json.error === 'browserToken not Found') {
+        DeviceManager.removeDevice(browserToken)
+      }
+    } catch (error) {
+      sendResponse(error)
+      console.error(error)
+    }
+    sendResponse('no errors downloading')
+  })
